56eb4610aacdcad1c6a51bba67e330aa5b0e01f6
Julien WALLE <jwalle@e2r10p22.42.fr>
2015-05-02 15:29:08 +0200

    .h

--- arkanoid.h
+++ arkanoid.h
@@ -6,7 +6,7 @@
 /*   By: kleiba <kleiba@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/05/01 20:29:57 by kleiba            #+#    #+#             */
-/*   Updated: 2015/05/02 12:00:01 by jwalle           ###   ########.fr       */
+/*   Updated: 2015/05/02 15:17:52 by jwalle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,31 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <fcntl.h>
+
 # include "libft/includes/libft.h"
 # include "glfw/include/GLFW/glfw3.h"
 
+typedef struct 		s_env
+{
+	int				**tab;
+	int				line;
+	int				col;
+	float 			x;
+}					t_env;
+
+typedef struct		s_brick
+{
+	float			xru;
+	float			yru;
+	float			xrd;
+	float			yrd;
+	float			xlu;
+	float			ylu;
+	float			xld;
+	float			yld;
+}					t_brick;
+
+void get_map(char *str, t_env *e);
+void	ft_follow(t_env *e);
 #endif
