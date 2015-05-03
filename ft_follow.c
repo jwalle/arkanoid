5a0ef8cd377a777899ad902cf0b30533e9a35fa5
Kevin LEIBA <kleiba@e2r11p23.42.fr>
2015-05-03 10:53:37 +0200

    ft_follow

--- ft_follow.c
+++ ft_follow.c
@@ -1,12 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_follow.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kleiba <kleiba@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/05/03 10:49:48 by kleiba            #+#    #+#             */
+/*   Updated: 2015/05/03 10:52:55 by kleiba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "arkanoid.h"
+
+void	ft_i_follow(double x, double y, double z)
+{
+	glBegin(GL_LINE_LOOP);
+	glColor3f(1.0f - z, 0.0f + z, 1.0f - z);
+	glVertex2d((-1.0f + x), (0.85f - y));
+	glVertex2d((-0.8f + x), (0.85f - y));
+	glVertex2d((-0.8f + x), (0.9f - y));
+	glVertex2d((-1.0f + x), (0.9f - y));
+	glEnd();
+}
+
+void	ft_he_follows(double x, double y, double z)
+{
+	glBegin(GL_QUADS);
+	glColor3f(0.0f - z, (1.0f - z), (0.0f + z));
+	glVertex2d((-1.0f + x), (0.85f - y));
+	glVertex2d((-0.8f + x), (0.85f - y));
+	glVertex2d((-0.8f + x), (0.9f - y));
+	glVertex2d((-1.0f + x), (0.9f - y));
+	glEnd();
+}
 
 void	ft_follow(t_env *e)
 {
-	int i;
-	int j;
-	double x;
-	double y;
-	double z;
+	int		i;
+	int		j;
+	double	x;
+	double	y;
+	double	z;
 
 	i = 0;
 	while (i < e->line)
@@ -19,55 +53,11 @@ void	ft_follow(t_env *e)
 			z = (e->tab[i][j] / 5.0);
 			if (e->tab[i][j] != 0)
 			{
-				glBegin(GL_QUADS);
-				glColor3f(0.0f - z,(1.0f - z),(0.0f + z));
-				glVertex2d((-1.0f + x), (0.85f - y)); // en bas à gauche
-				glVertex2d((-0.8f + x), (0.85f - y)); // en bas à droite
-				glVertex2d((-0.8f + x), (0.9f - y)); // en haut à droite
-				glVertex2d((-1.0f + x), (0.9f - y)); // en haut à gauche
-				glEnd();
-				glBegin(GL_LINE_LOOP);
-				glColor3f(1.0f - z, 0.0f + z, 1.0f - z);
-				glVertex2d((-1.0f + x), (0.85f - y)); // en bas à gauche
-				glVertex2d((-0.8f + x), (0.85f - y)); // en bas à droite
-				glVertex2d((-0.8f + x), (0.9f - y)); // en haut à droite
-				glVertex2d((-1.0f + x), (0.9f - y)); // en haut à gauche
-				glEnd();
+				ft_he_follows(x, y, z);
+				ft_i_follow(x, y, z);
 			}
 			j++;
 		}
 		i++;
 	}
 }
-// count -> x ; b -> y
-/*
-int		collide(t_env *e, int count, int b)
-{
-	double x;
-	double y;
-
-	x = (double)count;
-	y = (double)b;
-
-	while (i < e->line)
-	{
-		j = 1;
-		while (j < 11)
-		{
-			x = (j - 1) * 0.2;
-			y = i * 0.05;
-			if (e->tab[i][j] != 0)
-			{
-				
-					glVertex2d((-1.0f + x), (0.85f - y)); // en bas à gauche
-				//if ()
-					glVertex2d((-0.8f + x), (0.85f - y)); // en bas à droite
-
-					glVertex2d((-0.8f + x), (0.9f - y)); // en haut à droite
-					glVertex2d((-1.0f + x), (0.9f - y)); // en haut à gauche
-			}
-			j++;
-		}
-		i++;
-	}
-}*/
